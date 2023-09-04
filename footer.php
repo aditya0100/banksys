d9dc04adfba2366beb55081ad47673ce2bcf1e93
Aditya <adityarajyadav8@gmail.com>
2023-09-04 18:37:02 +0530

    completed version

---
+++ footer.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+<div class="container">
+    <div class="follow_us">
+        <label>Follow Us</label>
+        <ul>
+            <li><a href="https://www.facebook.com/">Facebook</a></li>
+            <li><a href="https://twitter.com/">Twitter</a></li>
+            <li><a href="https://www.instagram.com/">Instagram</a></li>
+    
+        </ul>
+    </div>
+    <div class="contact" id="contactus">
+        <label>Contact Us</label>
+        <ul>
+            <p>69 LC, Near Tara Public School,</p><p>Chutia, Ranchi 834001</p>
+            <p>Phone : +91 7717297714</p>
+            <p> Email: financebuddy@gmail.com</p>
+            
+        </ul>
+    </div>
+    <div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">About Us</a></li>
+            <li><a href="">Loan</a></li>
+            <!--li><a href="">Calculator</a></li--> 
+            </ul><br>
+    </div>
+
+        <!--div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">Link 1</a></li>
+            <li><a href="">Link 2</a></li>
+            <li><a href="">Link 3</a></li> 
+            </ul><br>
+        </div-->
+    
+    
+</div>
+    <div class="copyright">
+        <span>Copyright &copy; 2023 Finacial Buddy. All rights reserved.</span>
+    </div>
+    
+    <div class="bestview">
+        <span>Site best viewed at 1024 x 768 & above resolution in Microsoft Edge, Google Chrome 49+, Firefox 45+ and Safari 6+</span>
+    
+    </div>
+
+</footer>
